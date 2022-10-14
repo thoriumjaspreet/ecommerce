83f4a4d7faf786f32001b42682fd2c3265f79863
thoriumjaspreet <jaspreet98cheemaa@gmail.com>
2022-10-14 17:18:19 +0530

    complete

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export default {
+    MONGODB_URL : process.env.MONGODB_URL || "mongodb+srv://rohanDb:iamjerry@cluster0.etldx.mongodb.net/Ecomm-DB?retryWrites=true&w=majority",
+    JWT_SECRET : process.env.JWT_SECRET || "somethingsecret"
+}
---
+++ backend/routes/userRoute.js
@@ -0,0 +1,69 @@
+import express from "express";
+import User from "../models/userModel"
+
+const router = express.Router();
+
+router.post("/signin", async (req,res)=>{
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+    if(signinUser){
+      
+        res.send({
+            _id: signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin: signinUser.isAdmin,
+            token: getToken(signinUser)
+        })
+
+    } else {
+        res.status(401).send({msg: "Invalid Email or Password!"});
+
+    }
+})
+
+router.post("/register", async (req,res)=>{
+    const user = new User({
+       name: req.body.name,
+       email: req.body.email,
+       password: req.body.password
+
+    });
+    const newUser = await user.save();
+    if(newUser){      
+        res.send({
+            _id: newUser.id,
+            name: newUser.name,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,
+            token: getToken(newUser)
+        })
+
+    } else {
+        res.status(401).send({msg: "Invalid User Data!"});
+
+    }
+})
+
+
+
+router.get("/createadmin", async (req,res)=>{
+   try {
+    const user = new User({
+        name:"Bedran",
+        email:"bdrntrk@gmail.com",
+        password: "123456789",
+        isAdmin: true
+
+    });
+    const newUser = await user.save();
+    res.send(newUser);
+       
+   } catch (error) {
+       res.send({msg: error.message});
+   }
+  
+})
+export default router;
